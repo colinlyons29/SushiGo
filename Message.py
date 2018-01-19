4afe8fd95cb3593923ce0a99ba3f82ca00fa678e
jakub-piatek <35572501+jakub-piatek@users.noreply.github.com>
2018-01-19 16:25:22 +0000

    Add files via upload

--- Message.py
+++ Message.py
@@ -2,6 +2,8 @@
     are the basis of communication between the server and the clients.
     The file also contains two methods: compressMessage() and uncompressMessage()
     as well as a unit test.
+    Planning to add gzip compression to compressMessage() and uncompressMessage()
+    functions.
     Author: Jakub Piatek 115348936 """
 
 class Message(object):
@@ -25,23 +27,28 @@ class Message(object):
 def compressMessage(msg):
     """ Pickle and compress the Message object so that it can be sent across
         network. """
-    import gzip
+    #import gzip as gzip
     import pickle as pickle
 
     if not isinstance(msg, Message):
         return "Please only use Message object!"
 
-    outmsg = "foo"
+    outmsg = pickle.dumps(msg)
+
     return outmsg
 
 
 def uncompressMessage(msg):
     """ Uncompress and unpickle the Message object so that it can be processed
         by the receiver. """
-    import gzip
+    #import gzip as gzip
     import pickle as pickle
 
-    outmsg = "bar"
+    try:
+        outmsg = pickle.loads(msg)
+    except KeyError:
+        outmsg = "Please only use compressed Message object!"
+    
     return outmsg
 
 #------------------------- Testing -------------------------
@@ -54,17 +61,14 @@ def main():
     print(msg1)
     print("")
     cmsg = compressMessage(msg1)
-    print(cmsg)
-    print("")
     umsg = uncompressMessage(cmsg)
     print(umsg)
-    print(cmsg)
     print("")
     ecmsg = compressMessage("foobar")
     print(ecmsg)
     print("")
     eumsg = uncompressMessage("foobar")
-    print(ecmsg)
+    print(eumsg)
     print("")
 
 if __name__ == '__main__':
