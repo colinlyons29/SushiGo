33eb89f4864c2cf6ae4e2e1c69b7ff2f15d523a5
jakub-piatek <35572501+jakub-piatek@users.noreply.github.com>
2018-02-21 15:57:51 +0000

    Add files via upload

--- foo.py
+++ foo.py
@@ -1,55 +1,71 @@
 class Card(object):
-
-    def __init__(self, idd):
+    """ An object representation of a playing card.
+        The "Card" has three instance attributes:
+        @id is a unique identifier for each card
+        @cardType is used to represent different kinds of cards
+        @isTagged is used to mark card chosen by the player during each round """
+    def __init__(self, idd, cardType):
         self._id = idd
-        self._numberInDeck = 0
-        self._cardType = ""
+        self._cardType = cardType
         self._isTagged = False
+        #self._numberInDeck = 0
+
+    def __str__(self):
+        """ String representation of the card object. """
+        outstr = ""
+        outstr += "CardID: " + str(self._id)
+        outstr += " Card Type: " + str(self._cardType)
+        outstr += " Tagged: " + str(self._isTagged)
+        return outstr
 
     def getID(self):
         return self._id
 
-    def getIsTagged(self):
-        return self._isTagged
-
-    def getNumberInDeck(self):
-        return self._numberInDeck
-
-    def setNumberInDeck(self, number):
-        self._numberInDeck = number
-
     def getCardType(self):
         return self._cardType
 
-    def __str__(self):
-        return self._cardType + "| ID = " + str(self._id)
+    def getIsTagged(self):
+        return self._isTagged
+
+    def setIsTagged(self, isTagged):
+        self._isTagged = isTagged
+
+    #def getNumberInDeck(self):
+    #    return self._numberInDeck
+
+    #def setNumberInDeck(self, number):
+    #    self._numberInDeck = number
 
     #number = property(getNumberInDeck,setNumberInDeck)
     #cardType = property(getCardType)
 
-lstCards = [Card(i) for i in range(10)]
-for x in lstCards:
-    print(x)
+def constructDeck():
+    deck = []
+    return deck
 
-cards = dict()
+def main():
+    """ Sample test block. Will autorun only if code executed directly """
+    lstCards = [Card(i, "Maki") for i in range(10)]
+    for x in lstCards:
+        print(x)
 
-for card in lstCards:
-    cards[card.getID()] = [card.getNumberInDeck(), card.getCardType(), card.getIsTagged()]
-    #cards[card.getID()].append(card.getNumberInDeck())
-    #cards[card.getID()].append(card.getCardType())
+    cards = dict()
 
-cards[3][2] = True
+    for card in lstCards:
+        cards[card.getID()] = [card.getCardType(), card.getIsTagged()]
 
-for k in cards:
-    v = cards[k]
-    print("ID: " + str(k) + " Number in Deck: " + str(v[0]) + " Card Type: " + str(v[1]) + " Tagged: " + str(v[2]))
+    cards[3][1] = True
 
-import json
-
-outmsg = json.dumps(cards)
-inmsg = json.loads(outmsg)
-
-if inmsg is dict:
-    for k in inmsg:
+    for k in cards:
         v = cards[k]
-        print("ID: " + str(k) + " Number in Deck: " + str(v[0]) + " Card Type: " + str(v[1]) + " Tagged: " + str(v[2]))
+        print("ID: " + str(k) + " Card Type: " + str(v[0]) + " Tagged: " + str(v[1]))
+
+    import json
+
+    outmsg = json.dumps(cards)
+    inmsg = json.loads(outmsg)
+
+    if inmsg is dict:
+        for k in inmsg:
+            v = cards[k]
+            print("ID: " + str(k) + " Card Type: " + str(v[0]) + " Tagged: " + str(v[1]))
