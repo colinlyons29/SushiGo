e206d660f7545a09185b18da4e46299c97dfb76d
Colin Lyons <colinlyons29@gmail.com>
2018-03-07 11:34:46 +0000

    Bryan/final (#17)
    * Update Title
    
    * logout.py
    
    * Barebones register with validation
    
    * form outline
    
    * wipe branch
    
    * wipe branch
    
    * Bryan/selected cards (#16)
    
    * logout.py
    
    * Barebones register with validation
    
    * form outline
    
    * wipe branch
    
    * wipe branch
    
    * Title (#4)
    
    * Update Title
    
    * logout.py
    
    * Barebones register with validation
    
    * form outline
    
    * wipe branch
    
    * wipe branch
    
    * Card objects for each card type
    
    * Add files via upload
    
    * Bryan/player objects (#15)
    
    * Final Calculation Files
    
    Player, PlayerComparison, EndOfRoundCalculator, and EndOfGameCalculator are the only important ones here. Other files are used to simulate a game. The code in important files that reference the others will be replaced by intake from the network
    
    * Add files via upload
    
    * Delete PlayerComparison.py
    
    * Delete PremadeSelectedCards16.py
    
    * Delete PremadeSelectedCards4.py
    
    * Delete GameSimulator.py
    
    * Add files via upload
    
    * Delete PointCalculator.py
    
    * Delete PremadeSelectedCards.py
    
    * Bryan/player objects (#15) rebase
    
    * Update SelectedCards.py
    
    * Update SelectedCards.py

---
+++ SelectedCards.py
@@ -0,0 +1,15 @@
+from queue import *
+
+class SelectedCards(object):
+
+    def __init__(self):
+        self._cards = Queue()
+
+    def selectCard(self, card):
+        self._cards.put(card)
+
+    def draw(self):
+        return self._cards.get()
+
+    def size(self):
+        return self._cards.qsize()
