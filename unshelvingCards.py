b738812080c6cd8e3ef418c61fd00d33ba21e7a7
jakub-piatek <35572501+jakub-piatek@users.noreply.github.com>
2018-03-07 10:59:45 +0000

    Add files via upload

---
+++ unshelvingCards.py
@@ -0,0 +1,19 @@
+""" Takes in the name of the shelve as an argument and loads the information from
+    that shelve. The information is then stored in a json and passed to the 
+    program that called this script. """
+
+import shelve as shelve
+import json
+from sys import argv
+
+shelveName = argv[2]
+d = shelve.open(shelveName)
+tempD = dict()
+
+for k in d.keys():
+    tempD[k] = d[k]
+
+shelve.close()
+
+jsonObj = json.dumps(tempD)
+print(jsonObj)
