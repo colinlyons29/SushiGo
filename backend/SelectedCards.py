077d23155e2e27a97641e0d83df43da2f59beda2
Colin Lyons <colinlyons29@gmail.com>
2018-03-09 16:05:48 +0000

    cleanup

---
+++ backend/SelectedCards.py
@@ -0,0 +1,17 @@
+from queue import *
+
+class SelectedCards(object):
+#A class for storing all cards that a player has chosen in a round
+#Inherits from the pyhton in built queue as order matters
+
+    def __init__(self):
+        self._cards = Queue()
+
+    def selectCard(self, card):
+        self._cards.put(card)
+
+    def draw(self):
+        return self._cards.get()
+
+    def size(self):
+        return self._cards.qsize()
